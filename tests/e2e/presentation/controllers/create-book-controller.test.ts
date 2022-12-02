9493eae17b194ccf9039920f9bbaceeec381fe52
caiogmrocha <caioguilherme739@gmail.com>
2022-12-02 19:53:58 -0300

    style: organize testing directories

---
+++ tests/e2e/presentation/controllers/create-book-controller.test.ts
@@ -0,0 +1,5 @@
+import { describe, expect, it } from "vitest";
+
+describe('[E2E] Create Book Controller', () => {
+    it('should return 2', () => expect(1 + 1).toBe(2));
+});
--- vite-e2e.config.ts
+++ vite-e2e.config.ts
@@ -4,8 +4,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    dir: 'tests',
-    // environment: 'e2e',
+    include: ['**\/*.test.ts'],
     setupFiles: './tests/vitest-environment-e2e',
   },
 });
