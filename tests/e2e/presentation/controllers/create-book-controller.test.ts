0168bb189a995841cfc569084fdf04c2aeafbab7
caiogmrocha <caioguilherme739@gmail.com>
2022-12-02 20:11:19 -0300

    test: ensure CreateBookController returns 200 if book data has been saved in data source

--- tests/e2e/presentation/controllers/create-book-controller.test.ts
+++ tests/e2e/presentation/controllers/create-book-controller.test.ts
@@ -1,5 +1,29 @@
+import { ICreateBookControllerRequest, ICreateBookControllerRequestGenres } from "@/presentation/controllers/create-book-controller";
+
+import { app } from "@/main/app";
+import { faker } from '@faker-js/faker';
+import { randomInt } from "crypto";
+import request from 'supertest';
 import { describe, expect, it } from "vitest";
 
 describe('[E2E] Create Book Controller', () => {
-    it('should return 2', () => expect(1 + 1).toBe(2));
+    it('should return 200 if book has been created', async () => {
+        const bookData: ICreateBookControllerRequest = {
+            title: faker.random.word(),
+            description: faker.random.words(),
+            genre: [ "romance", "adventure", "horror" ][randomInt(3) - 1] as ICreateBookControllerRequestGenres,
+        };
+
+        const { body, status } = await request(app)
+            .post('/books')
+            .send(bookData);
+
+        expect(status).toBe(200);
+        expect(body).toEqual(expect.objectContaining({
+            id: expect.any(Number),
+            title: expect.any(String),
+            description: expect.any(String),
+            genre: expect.any(String),
+        }));
+    });
 });
